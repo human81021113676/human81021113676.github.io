0b1c8de68b98d129263547df3a2f15da77914659
Christopher Stabile <chrisstabile@C-P3O.fios-router.home>
2018-03-21 00:31:48 -0400

    Add date stamp to output filename

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function addDateStamp()
 {
     var d = new Date();
-    var curr_date = d.getDate();
-    var curr_month = d.getMonth() + 1; //Months are zero based
-    var curr_year = d.getFullYear();
-    return curr_year + curr_month + curr_date;
+    var dateStamp = d.getFullYear().toString() 
+                + (d.getMonth()+1).toString() 
+                + d.getDate().toString();
+    return dateStamp.slice(2);
 }
 
 function saveTextAsFile()
