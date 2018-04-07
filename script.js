4aca13224df161a73456f951c2be1a51577cf807
Christopher Stabile <chrisstabile@C-P3O.fios-router.home>
2018-04-07 18:52:20 -0400

    Textfile to html table TESTING--updated path to definitions.txt.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function txtToTable()
     }
     txtFile.send(null);
 
-    document.getElementById('pText').innerHTML = "hello";
+    document.getElementById('pText').innerHTML = line[0];
 }
 
 /* function addDateStamp()
